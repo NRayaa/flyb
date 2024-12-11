6d09208fd3094d439390ef15868a32bc845d689f
NRayaa <anassyihabuddin01@gmail.com>
2024-12-11 23:39:23 +0700

    fifth commit

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Title = () => {
     return (
         <div>
-            <div className="title h-[77vh]  pt-4 w-full relative px-8 lg:px-10 xl:px-10">
+            <div className="title h-[77vh]  pt-4 w-full relative px-8 lg:px-8 xl:px-8">
                 <div className="text-center">
                     <h1 className="text-[4.5rem] lg:text-[10rem] xl:text-[7rem] text-glow text-white font-[600]">
                         “where style takes flight!”.
