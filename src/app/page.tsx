5e18fc6de6f5a4cabca61dc6c19fbf2ecaaed46c
NRayaa <anassyihabuddin01@gmail.com>
2024-12-11 23:31:39 +0700

    forth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,8 +41,90 @@ const Home = () => {
             <Dividertwo></Dividertwo>
 
             {/* Step */}
-            <div className="steporder relative">
-                
+            <div className="steporder relative  w-full px-10 lg:px-20 pt-[2rem] lg:pt-[4rem] pb-[7rem] lg:pb-[12rem] overflow-hidden">
+                <div className="grid xl:grid-cols-3 relative z-30">
+
+                    <div className="w-full xl:w-[20rem] flex justify-center pb-[2rem] xl:pb-[0rem]">
+                        <div className="flex flex-col items-center text-center space-y-2 lg:space-y-0">
+                            <h2 className="text-[4rem] xl:text-[5rem] font-[500] leading-tight text-so">How to get our stuff ?</h2>
+                            <img src="img/17.png" className='w-[100%] sm:w-full h-auto' alt="" />
+                        </div>
+                    </div>
+                    <div className="col-span-1 lg:col-span-2">
+                        <div className="w-full rounded-[2rem] grid grid-cols-1 sm:grid-cols-4 card-so">
+                            <div className="col-span-1 w-full h-[8.5rem] px-5 py-4">
+                                <div className=" w-full img-card-so flex justify-center rounded-[1.5rem] h-full">
+                                    <img src="img/28.png" className='h-full w-auto' alt="" />
+                                </div>
+                            </div>
+                            <div className="col-span-1 sm:col-span-3 flex items-center pb-3 sm:pb-0 px-">
+                                <div className="px-5">
+                                    <p className='text-[1.5rem] text-white font-600'>Find Your Sparkle</p>
+                                    <p className='text-white'>Take a stroll through our collection and pick the beads that make your heart happy! When you’re ready, tap Let’s Chat to place your order.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-full grid grid-cols-4">
+                            <div className="col-span-1 w-full pl-5 pr-3 flex justify-center">
+                                <div className=" space-y-1">
+                                    <div className="w-1 h-1 img-card-so rounded-full"></div>
+                                    <div className="w-1 h-1 img-card-so rounded-full"></div>
+                                    <div className="w-1 h-1 img-card-so rounded-full"></div>
+                                    <div className="w-1 h-1 img-card-so rounded-full"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-full rounded-[2rem] grid grid-cols-1 sm:grid-cols-4 card-so">
+                            <div className="col-span-1 w-full h-[8.5rem] px-5 py-4">
+                                <div className=" w-full img-card-so flex justify-center rounded-[1.5rem] h-full">
+                                    <img src="img/29.png" className='h-full w-auto' alt="" />
+                                </div>
+                            </div>
+                            <div className="col-span-1 sm:col-span-3 flex items-center pb-3 sm:pb-0 px-">
+                                <div className="px-5">
+                                    <p className='text-[1.5rem] text-white font-600'>Find Your Sparkle</p>
+                                    <p className='text-white'>Take a stroll through our collection and pick the beads that make your heart happy! When you’re ready, tap Let’s Chat to place your order.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-full grid grid-cols-4">
+                            <div className="col-span-1 w-full pl-5 pr-3 flex justify-center">
+                                <div className=" space-y-1">
+                                    <div className="w-1 h-1 img-card-so rounded-full"></div>
+                                    <div className="w-1 h-1 img-card-so rounded-full"></div>
+                                    <div className="w-1 h-1 img-card-so rounded-full"></div>
+                                    <div className="w-1 h-1 img-card-so rounded-full"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-full rounded-[2rem] grid grid-cols-1 sm:grid-cols-4 card-so">
+                            <div className="col-span-1 w-full h-[8.5rem] px-5 py-4">
+                                <div className=" w-full img-card-so flex justify-center rounded-[1.5rem] h-full">
+                                    <img src="img/30.png" className='h-full w-auto' alt="" />
+                                </div>
+                            </div>
+                            <div className="col-span-1 sm:col-span-3 flex items-center pb-3 sm:pb-0 px-">
+                                <div className="px-5">
+                                    <p className='text-[1.5rem] text-white font-600'>Find Your Sparkle</p>
+                                    <p className='text-white'>Take a stroll through our collection and pick the beads that make your heart happy! When you’re ready, tap Let’s Chat to place your order.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="absolute w-auto  z-10  -bottom-0 lg:-top-[20rem] lg:-left-[5rem] sm:-left-20 -left-[5rem]">
+                    <img src="img/27.png" className="w-full h-full object-cover" alt="" />
+                </div>
+
+                <div className="absolute z-20 w-full left-0 bottom-0">
+                    <img src="img/32.png" className='' alt="" />
+                </div>
+
             </div>
 
 
